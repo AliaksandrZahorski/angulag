2ccd282c5b00b3f9c6564549ead889143e36f2dc
AliaksandrZahorski <alex.formail@gmail.com>
2020-12-28 22:13:33 +0300

    add binding

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
-@Component(
-  {
-    selector: 'app-card',
-    templateUrl: './card.component.html',
-    styleUrls: ['./card.component.css']
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+  title = 'Card title';
+  text = '';
+  description = 'This is example of some text';
+  pp: string = '';
+  getInfoBlock = () => 'that it';
+  handleChange = () => {
+    this.title = this.text;
+  };
+  handleInput = (event: any) => {
+    this.text = event.target.value;
+  };
+
+  ngOnInit(): void {
+    this.pp = 'my_class';
   }
-)
-export class CardComponent {
-  title = 'Card title'
 }
